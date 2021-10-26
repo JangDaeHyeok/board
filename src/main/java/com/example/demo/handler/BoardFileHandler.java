b20d81370a3e6f297f01278640d2127df9b00114
JangDaeHyeok <sktellecom@naver.com>
2021-10-26 14:36:49 +0900

    dd

--- src/main/java/com/example/demo/handler/BoardFileHandler.java
+++ src/main/java/com/example/demo/handler/BoardFileHandler.java
@@ -1,6 +1,9 @@
 package com.example.demo.handler;
 
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -24,19 +27,31 @@ public class BoardFileHandler {
 	@Transactional
 	@CircuitBreaker(name = BOARD_CIRCUIT_BREAKER, fallbackMethod = "fallback")
 	public Mono<ServerResponse> boardFileAdd(ServerRequest request) {
-//		return request.multipartData().map(it -> it.get("files"))
-//				.cast(FilePart.class)
-//				.flatMap(t -> {
-//					System.out.println(t);
-//					t.transferTo(Paths.get("/board/file" + t.filename()));
-//					return ServerResponse.ok().body(Mono.just("board file upload test"), String.class);
-//				});
+		request.multipartData().subscribe(st -> {
+			System.out.println(st.get("files"));
+			System.out.println(st.get("files"));
+			
+		});
+		request.multipartData().map(arg -> {
+			arg.forEach((key, val) -> {
+				System.out.println("key" + key);
+				System.out.println("val" + val);
+			});
+			return arg;
+		});
+		return request.multipartData().map(it -> it.get("files"))
+				.cast(FilePart.class)
+				.flatMap(t -> {
+					System.out.println(t);
+					t.transferTo(Paths.get("/board/file" + t.filename()));
+					return ServerResponse.ok().body(Mono.just("board file upload test"), String.class);
+				});
 //		 Mono<String> then = request.multipartData().map(it -> it.get("files"))
 //			        .flatMapMany(Flux::fromIterable)
 //			        .cast(FilePart.class)
 //			        .flatMap(it -> it.transferTo(Paths.get("/tmp/" + it.filename())))
 //			        .then(Mono.just("OK"));
-					return ServerResponse.ok().body(Mono.just("board file upload test"), String.class);
+//		return ServerResponse.ok().body(Mono.just("board file upload test"), String.class);
 	}
 	
 	
